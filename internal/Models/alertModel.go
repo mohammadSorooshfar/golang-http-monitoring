961e382c59d19cf4b2e11b3c4951321995b90af0
mahdy <mohamadnoori123987@gmail.com>
2022-09-18 19:47:02 +0430

    continue and end for handl get alerts for url

--- internal/Models/alertModel.go
+++ internal/Models/alertModel.go
@@ -6,7 +6,7 @@ import (
 
 type Alert struct {
 	ID     primitive.ObjectID `bson:"_id"`
-	Url    string             `query:"url"son:"url"`
+	Url    string             `query:"url"on:"url"`
 	Owner  string             `json:"owner"`
 	Time   string             `json:"time"`
 	UserId primitive.ObjectID `json:"user_id"`
